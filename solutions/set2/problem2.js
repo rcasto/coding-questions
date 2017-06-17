010fbc634462838cf1720e1a33ad839058fa386b
rcasto <rcasto@users.noreply.github.com>
2017-06-16 23:54:38 -0700

    updating code to revised problem, in progress

--- solutions/set2/problem2.js
+++ solutions/set2/problem2.js
@@ -1,37 +1,53 @@
-var linkedListNode = require('./linkedlist');
-
 function MostRecentList() {
-    this.recent = new linkedListNode();
     this.list = {};
-    this.counter = 0;
+    this.recent = new Set();
 }
-// Need a doubly linked list with references to both the head and the tail
 MostRecentList.prototype.add = function (str) {
-    var recentNode;
-    if (this.recent.data) {
-        recentNode = 
+    if (this.list[str]) {
+        this.list[str]++;
+    } else {
+        this.list[str] = 1;
     }
-    this.list[this.counter] = {
-
-    };
 };
-MostRecentList.prototype.remove = function (index) {
-    if (!this.list[index]) {
-        return false;
+MostRecentList.prototype.remove = function (str) {
+    if (this.list[str]) {
+        this.list[str]--;
+        if (!list[str]) {
+            this.recent.delete(str);
+            delete this.list[str];
+        }
+        return true;
     }
-
+    return false;
 };
-MostRecentList.prototype.get = function (index) {
-    return this.list[index];
+MostRecentList.prototype.removeAll = function (str) {
+    if (str) {
+        this.recent.delete(str);
+        delete this.list[str];
+    } else {
+        this.list = {};
+        this.recent.clear();
+    }
+};
+MostRecentList.prototype.has = function (str) {
+    return !!this.list[str];
+};
+MostRecentList.prototype.getNumEntriesOf = function (str) {
+    this.recent.
+    this.recent.add(str);
+    return this.list[str] || 0;
 };
 MostRecentList.prototype.getRecent = function () {
-
 };
 MostRecentList.prototype.logState = function () {
-
 };
 MostRecentList.prototype.toString = function () {
-
 };
+
+function updateRecent(recentSet, str) {
+    if (!recentSet.has(str)) {
+        recentSet.add(str);
+    }
+}
 
 modules.exports = MostRecentList;
