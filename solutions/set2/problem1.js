e36b30577bad66925af8e848999cc9b0a2962188
rcasto <rcasto@users.noreply.github.com>
2017-07-04 15:58:11 -0700

    Modify solution 1 of problem set 2, read from file instead of network

--- solutions/set2/problem1.js
+++ solutions/set2/problem1.js
@@ -1,49 +1,9 @@
-/*
-    This program right now will not spit out a constant consistent number for the # of distinct words in The Iliad
-    This is because of the stream processing, chunks do not necessarily end on a word.  They could cut off in the middle of the word.
-
-    Would need to stitch between chunks
-
-    loading all into memory, results in 7763 distinct words
-*/
-
 var http = require('http');
+var fs = require('fs');
 
 var distinctWordsSet = new Set();
 var unwantedCharactersSet = new Set(['\'', '!', '?', '-', ',', '.', ';', '\"', ':', '(', ')']);
-var oneLetterWordsSet = new Set(['a', 'i', 'o']);
-var chunkQueue = [];
-var isWorking = false;
-var isDoneReading = false;
-
-// loading all into memory
-var bigChunk = '';
-//
-
-function extractDistinct(chunk) {
-    return new Promise((resolve, reject) => {
-        chunk = chunk.replace(/\n/g, ' ');
-        var words = chunk.split(' ');
-        words.forEach((word) => {
-            word = word.toLowerCase();
-            word = removeUnwantedCharacters(word);
-            word = word.trim();
-            if (word && word.length === 1 && !oneLetterWordsSet.has(word)) {
-                word = '';
-            }
-            if (word && !distinctWordsSet.has(word)) {
-                // console.log(word, word.length);
-                distinctWordsSet.add(word);
-            }
-        });
-        resolve();
-    });
-}
-
-function report() {
-    var numDistinctWords = distinctWordsSet.size;
-    console.log(`Done reading, there are ${numDistinctWords} distinct words`);
-}
+var lastToken = '';
 
 function removeUnwantedCharacters(word) {
     var wordLength = word.length;
@@ -56,45 +16,102 @@ function removeUnwantedCharacters(word) {
     return cleanedWord;
 }
 
-var request = http.request({
-    protocol: 'http:',
-    port: 80,
-    method: 'GET',
-    host: 'classics.mit.edu',
-    path: '/Homer/iliad.mb.txt'
-}, (response) => {
-    response.setEncoding('utf8');
-    response.on('data', (chunk) => {
-        // load all into memory solution
-        bigChunk += chunk;
-        //
+function normalizeWordToken(token) {
+    token = token.toLowerCase();
+    token = removeUnwantedCharacters(token);
+    token = token.trim();
+    return token;
+}
 
-        // if (isWorking) {
-        //     chunkQueue.push(chunk);
-        // } else {
-        //     isWorking = true;
-        //     extractDistinct(chunk).then(function chunkCompletion() {
-        //         if (chunkQueue.length > 0) {
-        //             extractDistinct(chunkQueue.shift()).then(chunkCompletion);
-        //         } else if (isDoneReading) {
-        //             report();
-        //         } else {
-        //             isWorking = false;
-        //         }
-        //     });
-        // }
-    });
-    response.on('end', function () {
-        // load all into memory solution
-        extractDistinct(bigChunk).then(report);
-        //
+function stitchChunks(prevLastToken, currChunk) {
+    var words = currChunk.split(/\s+/);
+    var numWords = words.length;
+    var chunkLength = currChunk.length;
 
-        // isDoneReading = true;
-        // if (chunkQueue.length === 0 && !isWorking) {
-        //     report();
-        // }
+    // check first token if there was a previous last token saved
+    if (prevLastToken) {
+        if (chunkLength > 0 && currChunk[0] !== ' ') {
+            let save = words[0];
+            words[0] = prevLastToken + words[0]; // prepend the last saved token
+            console.log(prevLastToken, '+', save, '=', words[0]);
+        } else {
+            // console.log(prevLastToken);
+            words.push(prevLastToken); // last token was a word of it's own, add to list
+        }
+    }
+
+    // check last character, if not a space, let's save
+    // the last token it's connected to
+    if (currChunk[chunkLength - 1] !== ' ') {
+        prevLastToken = words[numWords - 1];
+    } else {
+        prevLastToken = ''; // reset saved token
+    }
+
+    return {
+        lastToken: prevLastToken,
+        words: words
+    };
+}
+
+function extractDistinct(chunk) {
+    var words, lastIndex;
+
+    // replace all newlines with a space character
+    chunk = chunk.replace(/\n|\r|\r\n/g, ' ');
+
+    ({ lastToken, words } = stitchChunks(lastToken, chunk));
+    lastIndex = words.length - 1;
+
+    words.forEach((word, i) => {
+        // Cannot be sure about last token without checking next token
+        // Implies last token when stream ends needs to be processed afterwards as it won't
+        // have a next token to check
+        if (i === lastIndex) {
+            return;
+        }
+
+        addDistinctWord(normalizeWordToken(word));
     });
+}
+
+function addDistinctWord(word) {
+    if (word && !distinctWordsSet.has(word)) {
+        distinctWordsSet.add(word);
+    }
+}
+
+function report() {
+    var numDistinctWords = distinctWordsSet.size;
+    console.log(`Done reading, there are ${numDistinctWords} distinct words`);
+}
+
+// Network request stream
+// var request = http.request({
+//     protocol: 'http:',
+//     port: 80,
+//     method: 'GET',
+//     host: 'classics.mit.edu',
+//     path: '/Homer/iliad.mb.txt'
+// }, (response) => {
+//     response.setEncoding('utf8');
+//     response.on('data', (chunk) => extractDistinct(chunk));
+//     response.on('end', () => {
+//         addDistinctWord(normalizeWordToken(lastToken));
+//         report();
+//     });
+// });
+// request.once('error', (error) => console.error(error));
+
+// request.end();
+
+// File stream
+var fileStream = fs.createReadStream('iliad.txt', {
+    encoding: 'utf8'
 });
-request.on('error', (error) => console.error(error));
-
-request.end();
+fileStream.on('data', (chunk) => extractDistinct(chunk));
+fileStream.on('end', () => {
+    addDistinctWord(normalizeWordToken(lastToken));
+    report();
+});
+fileStream.once('error', (error) => console.error(error));
